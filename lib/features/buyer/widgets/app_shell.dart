cfe3b897cb8cf439776abf684a353397e27daedc
hanan <hananfijananto@gmail.com>
2026-05-17 20:50:19 +0700

    fix: fix app shell and app bottom nav in buyer page.

---
+++ lib/features/buyer/widgets/app_shell.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tandur/features/buyer/widgets/app_bottom_nav.dart';
+
+class AppShell extends StatelessWidget {
+  final Widget child;
+  final String location;
+
+  const AppShell({super.key, required this.child, required this.location});
+
+  int _locationToIndex() {
+    if (location.startsWith('/buyer/profile')) {
+      return 3;
+    }
+    if (location.startsWith('/buyer/home')) {
+      return 0;
+    }
+    return 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: AppBottomNav(currentIndex: _locationToIndex()),
+    );
+  }
+}
